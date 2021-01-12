b42b36da6c9715f920845cd93c9db408992b99db
felipemouradev@gmail.com <felipe@Averys-MacBook-Air.local>
2021-01-12 14:54:28 -0600

    Finished

---
+++ List_exercises/Employees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace List_exercises
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employees(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee id: {Id}. Name: {Name}. Salary: {Salary.ToString("F2")}";
+        }
+
+        public void Raise(double percent)
+        {
+            Salary *= (percent * 0.01 + 1);
+        }
+    }
+}
---
+++ List_exercises/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace List_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many employees will be registered?");
+            int? n = int.Parse(Console.ReadLine());
+            var fs = new List<Employees>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i+1}");
+
+                Console.Write("Id? ");
+                int myId = int.Parse(Console.ReadLine());
+
+                Console.Write("Name? ");
+                string myName = Console.ReadLine();
+
+                Console.Write("Salary? ");
+                double mySalary = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                fs.Add(new Employees(myId, myName, mySalary));
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            var in_id = int.Parse(Console.ReadLine());
+
+            var my_func = fs.Find(x => in_id == x.Id);
+            if (my_func == null) {
+                Console.WriteLine("Employee not found.");
+            } else
+            {
+                Console.Write("Enter the percentage: ");
+                my_func.Raise(double.Parse(Console.ReadLine()));
+                Console.WriteLine(my_func);
+            }
+        }
+    }
+}
